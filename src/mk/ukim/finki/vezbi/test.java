47a65e63d406046b8cf8924b4ee313cf0048c50d
ManuelTrajcev <manuel.trajchev@students.finki.ukim.mk>
2024-03-01 13:51:41 +0100

    1.3.2024

---
+++ src/mk/ukim/finki/vezbi/test.java
@@ -0,0 +1,9 @@
+package mk.ukim.finki.vezbi;
+
+public class test {
+    public static void main(String[] args) {
+        double d = 0.123456789123456789;
+        System.out.println(d);
+        System.out.printf("%f", d);
+    }
+}
